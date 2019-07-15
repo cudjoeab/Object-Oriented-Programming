39ab0525b5bb199f337395d30e241e18295e1b6b
cudjoeab <cudjoeab@gmail.com>
2019-07-15 16:41:10 -0400

    instance method for meal time

--- Cats_Meow/03-OOP-cat.py
+++ Cats_Meow/03-OOP-cat.py
@@ -6,9 +6,28 @@ class Cat:
         self.name = name
         self.preferred_food = preferred_food
         self.meal_time = meal_time
+
+    def __str__(self):
+        return f"Please feed {self.name} some {self.preferred_food} at {self.meal_time}PM. "
+    #instance method for meal time 
+    def eats_at(self):
+        if self.meal_time > 0 and self.meal_time < 12: 
+            return str(f'Meal time is at {self.meal_time}AM.')
+        if self.meal_time > 12 and self.meal_time < 24:
+            self.meal_time -= 12 
+            return str(f'Meal time is at {self.meal_time} PM.')
     
+    
+
 
 #two instances of Cat 
 garfield = Cat('Garfield', 'lasagna', 17)
 hugo = Cat('hugo', 'jerk chicken', 16)
+
+print(garfield)
+print(hugo)
+
+garfield.eats_at()
+print(garfield)
+
 
