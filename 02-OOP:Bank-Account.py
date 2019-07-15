414eb78434a04316863ff03007f3df8dc8ff15c4
cudjoeab <cudjoeab@gmail.com>
2019-07-15 16:10:18 -0400

    bank account exercise

--- 02-OOP:Bank-Account.py
+++ 02-OOP:Bank-Account.py
@@ -3,6 +3,35 @@ class BankAccount:
     def __init__(self, balance, interest_rate):
         self.balance = balance
         self.interest_rate = interest_rate
+    
+    def __str__(self):
+        return(f"Your balance is {self.balance} at an interest rate of {self.interest_rate}.")
 
-account 1 = (BankAccount ())
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount): 
+        self.balance -= amount
+
+    def gain_interest(self):
+        interest = self.balance * self.interest_rate
+        self.balance += interest
+        return(f"Your total balance is {self.balance}.")
+
+account1 = BankAccount(1000, 0.16)
+print(account1)
+
+account1.deposit(50)
+print(account1)
+
+account1.withdraw(50)
+print(account1)
+
+account1.gain_interest()
+print(account1)
+
+
+
+
+
 
