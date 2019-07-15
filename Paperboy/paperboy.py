734a6e0114122c3c5ebf521fc8409a3091a15a26
cudjoeab <cudjoeab@gmail.com>
2019-07-15 19:42:10 -0400

    added string method

--- Paperboy/paperboy.py
+++ Paperboy/paperboy.py
@@ -3,6 +3,18 @@ class Paperboy:
         self.name = name
         self.experience = experience
         self.earnings = earnings
+    
+#String method 
+    def __str__(self):
+        return f"{self.name} has delivered {self.experience} papers and made ${self.earnings}."
 
-jack = Paperboy('Jack', 100, 10.00)
+
+
+
+#print information about paperboy 
+jack = Paperboy('Jack', 100, 25.00)
+les = Paperboy('Les', 50, 6.25)
 print(jack)
+print(les)
+
+
