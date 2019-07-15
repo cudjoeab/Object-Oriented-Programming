346249f992a720e9f3c4794a588719417c1d07f0
cudjoeab <cudjoeab@gmail.com>
2019-07-15 17:16:32 -0400

    default values added to constructor

--- Player/04-player.py
+++ Player/04-player.py
@@ -1,5 +1,8 @@
 class Player: 
-
-    def __init__(self, gold_coins, health_points_lives): 
+    
+    def __init__(self, gold_coins = 0, health_points = 10, lives = 5): 
         self.gold_coins = gold_coins
-        
+        self.health_points = health_points 
+        self.lives = lives 
+
+    
