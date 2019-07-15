6cb0de1debf5867278dc0dbe072b84c714187273
cudjoeab <cudjoeab@gmail.com>
2019-07-15 17:25:05 -0400

    added name argument and first player

--- Player/04-player.py
+++ Player/04-player.py
@@ -1,8 +1,13 @@
 class Player: 
     
-    def __init__(self, gold_coins = 0, health_points = 10, lives = 5): 
+    def __init__(self, name, gold_coins = 0, health_points = 10, lives = 5): 
+        self.name = name 
         self.gold_coins = gold_coins
         self.health_points = health_points 
         self.lives = lives 
 
-    
+    def __str__(self):
+        return f'{self.name} has {self.gold_coins} gold coins, {self.health_points} health points, and {self.lives} lives.'
+
+player1 = Player('Abigail')
+print(player1)
