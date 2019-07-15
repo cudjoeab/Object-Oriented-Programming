a856c8d07f14fdd56d064f58fbd1b269066d9672
cudjoeab <cudjoeab@gmail.com>
2019-07-15 18:05:10 -0400

    battle and reset functions

--- Player/04-player.py
+++ Player/04-player.py
@@ -14,9 +14,22 @@ class Player:
 
  #collect coins and level up   
     def collect_treasure(self, coins):
-        self.gold_coins += 1 
+        self.gold_coins += 1
         if self.gold_coins % 10 == True:
-            player1.level_up(1)
+            self.level_up(1)
+
+#do battle
+    def do_battle(self, damage):
+        self.health_points -= damage
+        if self.health_points < 1:
+            self.lives -= 1 
+            self.health_points = 10
+        if self.lives == 0:
+            self.player1(restart())
+    
+#restart game 
+    def restart(self, gold_coins, health_points, lives):
+        player1(__init__)
 
 
 player1 = Player('Abigail')
@@ -25,3 +38,7 @@ print(player1)
 # #calling level up instance method 
 player1.level_up(1)
 print(player1)
+
+player1.collect_treasure(2)
+print(player1)
+
