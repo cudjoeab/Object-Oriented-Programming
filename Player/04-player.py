8b1edce794a6aea1fef32d67f57813bea82f5e81
cudjoeab <cudjoeab@gmail.com>
2019-07-15 17:51:47 -0400

    collect coins method and level up condition

--- Player/04-player.py
+++ Player/04-player.py
@@ -8,6 +8,20 @@ class Player:
 
     def __str__(self):
         return f'{self.name} has {self.gold_coins} gold coins, {self.health_points} health points, and {self.lives} lives.'
+#level up 
+    def level_up(self, lvl_up):
+        self.lives += lvl_up
+
+ #collect coins and level up   
+    def collect_treasure(self, coins):
+        self.gold_coins += 1 
+        if self.gold_coins % 10 == True:
+            player1.level_up(1)
+
 
 player1 = Player('Abigail')
+print(player1)
+
+# #calling level up instance method 
+player1.level_up(1)
 print(player1)
