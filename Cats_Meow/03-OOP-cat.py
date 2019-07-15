600f66ad82fbb91c93186901a1f70c6e78fb0379
cudjoeab <cudjoeab@gmail.com>
2019-07-15 16:18:40 -0400

    create two instances of Cat

--- Cats_Meow/03-OOP-cat.py
+++ Cats_Meow/03-OOP-cat.py
@@ -1,3 +1,14 @@
 # Create a class called 'cat'
 
 class Cat: 
+
+    def __init__(self, name, preferred_food, meal_time):
+        self.name = name
+        self.preferred_food = preferred_food
+        self.meal_time = meal_time
+    
+
+#two instances of Cat 
+garfield = Cat('Garfield', 'lasagna', 17)
+hugo = Cat('hugo', 'jerk chicken', 16)
+
